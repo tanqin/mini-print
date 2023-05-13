2aed659e34f9564bf37072ca3d70c981af487c49
tanqin <cn.tanqin@qq.com>
2023-05-13 11:35:28 +0800

    chore: 配置 lint-staged

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,20 +4,20 @@ module.exports = {
     browser: true,
     es2021: true,
     node: true,
-    jest: true,
+    jest: true
   },
   extends: [
-    "eslint:recommended", // eslint
-    "plugin:prettier/recommended", // plugin-prettier
-    "plugin:vue/vue3-essential", // plugin-vue
+    'eslint:recommended', // eslint
+    'plugin:prettier/recommended', // plugin-prettier
+    'plugin:vue/vue3-essential' // plugin-vue
   ],
   parserOptions: {
-    parser: "@babel/eslint-parser", // 解析器
-    ecmaVersion: "latest",
-    sourceType: "module",
+    parser: '@babel/eslint-parser', // 解析器
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   rules: {
-    "space-before-function-paren": ["error", "never"], // 函数名和左括号之间不应该有空格,
-    semi: 0, // 结尾不要分号
-  },
-};
+    'space-before-function-paren': ['error', 'never'], // 函数名和左括号之间不应该有空格,
+    semi: 0 // 结尾不要分号
+  }
+}
